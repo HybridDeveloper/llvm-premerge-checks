38f50f607bbfdb13d053fed6a01c0d6e2080e0c8
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2020-05-28 17:03:25 +0000

    .

--- containers/agent-windows-buildkite/start_agent.ps1
+++ containers/agent-windows-buildkite/start_agent.ps1
@@ -3,7 +3,8 @@ c:\credentials\buildkite-env.ps1
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/buildkite/agent/master/install.ps1'))
 
 $env:SCCACHE_DIR="C:\ws\sccache"
-Remove-Item -Recurse -Force $env:SCCACHE_DIR
+
+Remove-Item -Recurse -Force -ErrorAction Ignore $env:SCCACHE_DIR
 # needed for jenkins likely $env:SCCACHE_IDLE_TIMEOUT="0"
 sccache --start-server
 
